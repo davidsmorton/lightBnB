13cd188aa3e86efce2b1a245dae2917c4e89c83c
davidsmorton <davidsmorton@gmail.com>
2020-10-16 01:28:42 +0000

    updated getAllProperties to allow for searching by city, price, and rating

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -106,9 +106,59 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
- return pool
-  .query(`SELECT * FROM properties LIMIT $1`, [limit])
-  .then(res => res.rows) 
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE 1 = 1
+  `;
+
+  // 3 ADD AND Statments?
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += ` AND city LIKE $${queryParams.length}`;
+  }
+  
+  if (options.owner_id) {
+    queryParams.push(Number(options.owner_id));
+    queryString += ` AND owner_id =  $${queryParams.length}`;
+  }
+  
+  if (options.minimum_rating) {
+    queryParams.push(Number(options.minimum_rating));
+    queryString += ` AND rating > $${queryParams.length}`;
+  }
+  
+  if (options.minimum_price_per_night) {
+    queryParams.push(Number(options.minimum_price_per_night)*100);
+    queryString += ` AND cost_per_night > $${queryParams.length}`;
+  }
+  if (options.maximum_price_per_night) {
+    queryParams.push(Number(options.maximum_price_per_night)*100);
+    queryString += ` AND cost_per_night < $${queryParams.length}`;
+  }
+// edge case only min price
+// edge case only max price
+// confirm not using else if ....because we want to consider all conditions
+
+
+  // 4 This doesn't look like it needs to be changed ask
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams)
+  .then(res => res.rows)
   .catch(err => console.error('query error', err.stack));
 }
 exports.getAllProperties = getAllProperties;
